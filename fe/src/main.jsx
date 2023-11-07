4b1a8ec9e0ebf5bb474db9c2ef91bccd2b8c286f
Michal Vanek <michvanek@gmail.com>
2023-11-07 05:24:07 +0100

    css adjustments

--- fe/src/main.jsx
+++ fe/src/main.jsx
@@ -11,6 +11,9 @@ import "@fortawesome/fontawesome-free/css/all.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
+//fonts
+import "@fontsource/roboto";
+
 // import $ from "jquery";
 
 createRoot(document.getElementById("root")).render(
